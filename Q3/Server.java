1c99ac10d516238a347ad4a382562d3a22c31c6e
ArnoBidet <arno.bidet479@gmail.com>
2025-09-22 11:38:40 -0400

    Add server functionnalities for calculator, register services in RMIC

--- Q3/Server.java
+++ Q3/Server.java
@@ -1,6 +1,23 @@
+import java.rmi.Naming;
+
+import calculator.*;
+
 public class Server implements AgentInterface {
     @Override
     public void start() {
-        System.out.println("Vous avez choisi de lancer un serveur.");
+        System.out.println("Vous avez lancé un serveur.");
+        System.setSecurityManager(new SecurityManager());
+        try {
+            Adder ai = new AdderImpl();
+            Substractor si = new SubstractorImpl();
+            Multiplier mi = new MultiplierImpl();
+            Divider di = new DividerImpl();
+            Naming.rebind("adder", ai);
+            Naming.rebind("substractor", si);
+            Naming.rebind("multiplier", mi);
+            Naming.rebind("divider", di);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
