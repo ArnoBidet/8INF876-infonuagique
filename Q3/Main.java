2d28a998d074fec32bef6db3441ea1f5784156f4
ArnoBidet <arno.bidet479@gmail.com>
2025-09-22 11:36:37 -0400

    Dead whitespace

--- Q3/Main.java
+++ Q3/Main.java
@@ -5,7 +5,7 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // 
+        //
         int result = 0;
         do {
             result = getUserInput();
